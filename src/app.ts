dbce335cba804b8455ce8acb479a3ecc51a12aa9
JuanBona <jbonanno@frro.utn.edu.ar>
2025-05-05 12:33:49 -0300

    ajustando puertos

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ app.use((_, res) => {
 
 //await syncSchema() //never in production. This is for development only
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(` Servidor corriendo en http://localhost:${PORT}`);
 });
